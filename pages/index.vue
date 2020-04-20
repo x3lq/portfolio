48cc6f7d8915cedcfb7c0e4ef08448e0502632ca
x3lq <webmaster@x3lq.de>
2020-04-20 20:57:55 +0200

    basic setup of nuxt frontend app

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
